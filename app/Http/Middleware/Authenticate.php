0eb2dda5775c57f0ba1cbb2a4a5138ce1f15b6d4
Ibrahim Ahmed <u1350260@unimail.hud.ac.uk>
2016-02-20 16:35:05 +0000

    Login and Register Form
    Made improvements to the applications routes. Login and register form
    work, user logs in and stays logged in.  Displays the logged users name.
    Reset password needs tinkering around.

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,15 +12,16 @@ class Authenticate
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
+     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->guest()) {
-            if ($request->ajax() || $request->wantsJson()) {
+            if ($request->ajax()) {
                 return response('Unauthorized.', 401);
             } else {
-                return redirect()->guest('users/login');
+                return redirect()->guest('login');
             }
         }
 
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
 /*
@@ -27,34 +24,12 @@ Route::get('/', function () {
 |
 */
 
-
-/* User Authentication */
-Route::get('users/login', 'Auth\AuthController@getLogin');
-Route::post('users/login', 'Auth\AuthController@postLogin');
-Route::get('users/logout', 'Auth\AuthController@getLogout');
-
-Route::get('users/register', 'Auth\AuthController@getRegister');
-Route::post('users/register', 'Auth\AuthController@postRegister');
-
-/* Authenticated users */
-Route::group(['middleware' => 'auth'], function()
-{
-    Route::get('users/dashboard', array('as'=>'dashboard', function()
-	{
-	return View('users.dashboard');
-	}));
-});
-
-
-
-Route::group(['middleware' => ['web']], function () {
-    //
-});
-
-
-
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
+
+    Route::get('/', function () {
+    return view('welcome');
+});
 
     Route::get('/home', 'HomeController@index');
 });
