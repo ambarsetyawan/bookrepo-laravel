c21a325be9436a4f725d4b7677a4332c9120ee84
Ibrahim Ahmed <u1350260@unimail.hud.ac.uk>
2016-02-25 14:10:12 +0000

    Dashboard and welcome screen changes
    Adjusted the routes for dashboard and welcome screen. Added to text to
    welcome screen to introduce what the website is about.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,7 +6,7 @@
 |--------------------------------------------------------------------------
 |
 | Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
+| It's a breeze. Simply tell Laravel the URIs it should respond t
 | and give it the controller to call when that URI is requested.
 |
 */
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,24 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Welcome to Bookrepo! </div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    On this Website you can browse books and find information about them. Such as:</p>
+                    - Summary and titles,</p>
+                    - Description</p>
+                    - Authurs</p>
+                    - Ratings</p>
+                    - Reviews</p>
+                    - and links to buy from retail websites.</p>
+                  </br>
+
+
+
+                  Important Information:</p>
+                    * As a regular user, you can only browse books, read the information and click on links.</p>
+                    * But if you are a registered user, you can have the advantages of adding comments and rating the books.
+
                 </div>
             </div>
         </div>
